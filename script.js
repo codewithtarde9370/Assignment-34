1570f093730dacf54c0e9ccc967801751291ade7
codewithtarde9370 <teja36491@gmail.com>
2024-04-25 22:39:12 +0530

    changes added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const todoList= []
+const todoList= [];
+function add(){
+    const todoArr = document.getElementById("todo-input").value;
+    todoList.push(todoArr);
+}
+function renderlist{
+    const todolistcont = document.getElementById("list-cont");
+    
+    if ()
+}
