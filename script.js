b63b00683f2fe665e339b297740d23cbc480d1e2
codewithtarde9370 <teja36491@gmail.com>
2024-04-27 12:51:16 +0530

    change in the code

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const todoList= [];
-function add(){
-    const todoArr = document.getElementById("todo-input").value;
-    todoList.push(todoArr);
-}
-function renderlist{
-    const todolistcont = document.getElementById("list-cont");
-    
-    if ()
-}
+// const todoList= [];
+
+// function add(){
+//     const todoInput = document.getElementById("task");
+//     todoList.push(todoInput.value);
+//     todoInput.value = "";
+//     renderlist();
+// }
+// function renderlist(){
+//     const todolistcont = document.getElementById("list-cont");
+//     todolistcont.innerHTML="";
+
+//     for(const todoItem of todoList)
+//     {
+//         todolistcont.innerHTML += `<li class="todoItem"> ${todoItem} </li>` + "<br>";
+//     }
+// }
+// function removeTodo(index) {
+//     todoList.splice(index, 1);
+//     displayTodoList();
+// }
